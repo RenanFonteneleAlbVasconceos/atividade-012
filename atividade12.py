c1d6617fd8b39612c5b68896c51603f70827bc71
RenanFonteleAlbVasconcelos <renan.vasconcelos3@aluno.ce.gov.br>
2025-10-15 13:59:54 -0300

    Mostra apenas números pares numa lista de 1 á 10

--- atividade12.py
+++ atividade12.py
@@ -1,4 +1,6 @@
 # Crie uma lista de números de 1 a 10 e mostre apenas os números pares.
 
 numeros = [1,2,3,4,5,6,7,8,9,10]
-print(numeros[1],numeros[3],numeros[5],numeros[7],numeros[9])
+for n in range(1, 11):
+    if n % 2 == 0:
+        print(n)
